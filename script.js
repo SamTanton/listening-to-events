fb31eee5eb2e02bda7c6f7e0fe1cd2228936e343
SamTanton <samt@unmade.com>
2023-06-12 12:54:35 +0100

    Inner box colour changes

--- script.js
+++ script.js
@@ -5,8 +5,22 @@ const bodyChangeColour = () => {
     topBox.innerHTML = `#${randomColour}`;
 }
 
+const boxChangeColour = () => {
+    const box = document.querySelector(".box")
+    const randomColour = Math.floor(Math.random()*16777215).toString(16);
+    box.style.backgroundColor = `#${randomColour}`;
+    box.innerHTML = `#${randomColour}`;
+}
+
+
 const outerbutton = document.querySelector(".outerbutton")
 
 outerbutton.addEventListener("click", () => {
 bodyChangeColour()
 })
+
+const innerbutton = document.querySelector(".innerbutton")
+
+innerbutton.addEventListener("click", () => {
+    boxChangeColour()
+    })
