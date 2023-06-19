676f84a20cbb16628328a8e8c741630e23c9c666
SamTanton <samt@unmade.com>
2023-06-19 20:23:25 +0100

    Addition of event mouseover and mouseout listeners

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const outerbutton = document.querySelector(".outerbutton")
-const innerbutton = document.querySelector(".innerbutton")
 
 outerbutton.addEventListener("click", () => {
     const topBox = document.querySelector(".topBox")
@@ -8,9 +7,25 @@ outerbutton.addEventListener("click", () => {
     topBox.innerHTML = `#${randomColour}`;
 })
 
+const innerbutton = document.querySelector(".innerbutton")
+
 innerbutton.addEventListener("click", () => {
     const box = document.querySelector(".bottomBox")
     const randomColour = Math.floor(Math.random()*16777215).toString(16);
     box.style.backgroundColor = `#${randomColour}`;
     box.innerHTML = `#${randomColour}`;
+})
+
+const circleShape = document.querySelector(".circleShape")
+
+const circleText = document.querySelector(".circleText")
+
+circleShape.addEventListener("mouseover", () => {
+    circleText.innerText = `You are inside me!`;
+    circleShape.style.backgroundColor = `#53FFFB`;
+})
+
+circleShape.addEventListener("mouseout", () => {
+    circleText.innerText = `Please get inside me again! I miss you`;
+    circleShape.style.backgroundColor = `#9aade1`;
 })
